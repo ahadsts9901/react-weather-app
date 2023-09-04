1900f4512c46c1628456301ad71eb52179d10d30
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-09-04 18:31:24 +0500

    done

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,7 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import WeatherCard from "../weatherWidget/weatherWidget";
-import "./home.css"
+import './home.css'
 
 const Home = () => {
   // not recommended
@@ -10,27 +10,74 @@ const Home = () => {
   const [weatherData, setWeatherData] = useState([]);
   const cityNameRef = useRef(null);
 
+  const [currentLocationWeather, setCurrentLocationWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const controller = new AbortController();
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (location) => {
+        console.log("location: ", location);
+
+        try {
+          let API_KEY = "e0f99c494c2ce394a18cc2fd3f100543";
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${API_KEY}&units=metric`,
+            {
+              signal: controller.signal,
+            }
+          );
+          console.log(response.data);
+
+          setCurrentLocationWeather(response.data);
+          setIsLoading(false);
+        } catch (error) {
+          console.log(error.data);
+          setIsLoading(false);
+        }
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+
+    return () => {
+      // cleanup function
+      controller.abort();
+    };
+  }, []);
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("cityName: ", cityNameRef.current.value);
+    // console.log("cityName: ", cityNameRef.current.value);
 
-    let API_KEY = '26ae9a0b27a97215beaa4b5df9aa15fb';
+    let API_KEY = "e0f99c494c2ce394a18cc2fd3f100543";
     try {
+      setIsLoading(true);
+
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityNameRef.current.value}&appid=${API_KEY}&units=metric`
       );
 
-      console.log(response.data);
+      // console.log(response.data);
       setWeatherData([response.data, ...weatherData]);
+      setIsLoading(false);
+      cityNameRef.current.value = ""
     } catch (error) {
       // handle error
-      console.log(error.data);
+      console.log(error?.data);
+      setIsLoading(false);
     }
   };
 
   return (
     <div>
-      <form onSubmit={submitHandler} className="flex justify-between items-center p-[0.5em] w-[100%] rounded-[10px] bg-[#fdfdfd]">
+      
+      <div className="head bg-[#171d2501] flex flex-col justify-right items-center gap-[1em] sticky top-0 z-20">
+      <h1 className="z-[30] mb-[1em] flex justify-center itens-center gap-[0.5em] text-center w-[100%] text-[#fec55e] bi bi-cloud-sun text-[1.5em]"><span className="text-[#fff]">Weather</span><span className="text-[#ff6677]">App</span></h1>
+      <form onSubmit={submitHandler} className="z-[30] mb-[1em] w-[100%] flex justify-center items-center gap[1em] bg-[#fff] p-[0.5em] rounded-[15px]">
         <input
           id="cityNameInput"
           type="text"
@@ -39,20 +86,25 @@ const Home = () => {
           maxLength={20}
           //   onChange={(e) => setCityName(e.target.value)}
           ref={cityNameRef}
+          className="bg-[#fff] p-[0.5em] w-[100%]"
+          placeholder="Enter City Name..."
         />
         <br />
-        <button type="submit">Get Weather</button>
+        <button type="submit" className="text-[#fff] bg-[#ff6677] p-[0.5em] rounded-[10px]">Search</button>
       </form>
+      </div>
 
-      <hr />
+      <div className="result p-[1em] w-[100%] flex flex-wrap justify-center items-start h-[100%]">
 
-      {weatherData.length ? (
-        weatherData.map((eachWeatherData, index) => {
+        {weatherData.length || currentLocationWeather || isLoading ? null : <div>No Data</div>}
+
+        {weatherData.map((eachWeatherData, index) => {
           return <WeatherCard key={index} weatherData={eachWeatherData} />;
-        })
-      ) : (
-        <div>No Data</div>
-      )}
+        })}
+
+        {currentLocationWeather ? <WeatherCard weatherData={currentLocationWeather} /> : null}
+      </div>
+
     </div>
   );
 };
--- src/components/weatherWidget/weatherWidget.jsx
+++ src/components/weatherWidget/weatherWidget.jsx
@@ -2,19 +2,20 @@ import "./weatherWidget.css";
 
 const WeatherCard = ({ weatherData }) => {
   return (
-    <div className="card">
-      city name: {weatherData?.name}
-      <br />
-      country: {weatherData?.sys?.country}
-      <br />
-      <div className="temp">{weatherData?.main?.temp}°C</div>
-      <br />
-      humidity: {weatherData?.main?.humidity}
-      <br />
-      wind speed: {weatherData?.wind?.speed}
-      <br />
-      weather: {weatherData?.weather[0]?.description}
+    <div className="card text-[#fff] flex flex-col justify-right gap-[1em] items-start">
+      <h1 className="ov-x text-center w-[100%] text-[1.3em] text-[#ff6677]">{weatherData.name}</h1>
+      <h1 className="text-[2em] w-[100%] text-center text-[#fec55e]">{weatherData?.main.temp} °C</h1>
+      <div className="flex justify-center gap-[1em] items-center w-[100%] text-[#ff6677]">
+        <p className="text-[#fff]">{weatherData.sys.country}</p>
+        <p>{weatherData.weather[0]?.description}</p>
+      </div>
+      <p className="text-[0.8em] w-[100%] text-left text-[#fff] flex justify-left items-center gap-[1em]"><p className="bi bi-droplet"></p><p>Humidity : {weatherData.main.humidity} °C</p></p>
+      <p className="text-[0.8em] w-[100%] text-left text-[#fff] flex justify-left items-center gap-[1em]"><p className="bi bi-wind"></p><p>Wind : {(((weatherData.wind.speed) * 2.9).toFixed())} km/h</p></p>
+      <p className="text-[0.8em] w-[100%] text-left text-[#fff] flex justify-left items-center gap-[1em]"><p className="bi bi-hurricane"></p><p>Pressure : {weatherData.main.pressure} Pascals</p></p>
+      <p className="text-[0.8em] w-[100%] text-left text-[#fff] flex justify-left items-center gap-[1em]"><p className="bi bi-thermometer-half"></p><p> Min Temp : {weatherData.main.temp_min} °C</p></p>
+      <p className="text-[0.8em] w-[100%] text-left text-[#fff] flex justify-left items-center gap-[1em]"><p className="bi bi-thermometer-high "></p><p>Max Temp : {weatherData.main.temp_max} °C</p></p>
     </div>
+
   );
 };
 
